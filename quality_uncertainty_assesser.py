33000642a0c43c4103e5324f3bf4436d2a05e0d3
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2024-05-17 13:23:41 +0300

    Refactor for base folder

--- quality_uncertainty_assesser.py
+++ quality_uncertainty_assesser.py
@@ -3,58 +3,58 @@ import json
 import os
 
 def get_sboms_for_images(image_name: str):
-    command_syft_sbom = ["syft", "--output", "spdx-json", "--file", os.path.join("results", image_name, "sbom_syft.json"), image_name]
+    command_syft_sbom = ["syft", "--output", "spdx-json", "--file", os.path.join(base_folder, image_name, "sbom_syft.json"), image_name]
     subprocess.run(command_syft_sbom, capture_output=True, text=True, check=True)
 
-    command_grype_sbom = ["grype", "--output", "json", "--file", os.path.join("results", image_name, "json_grype.json"), image_name]
+    command_grype_sbom = ["grype", "--output", "json", "--file", os.path.join(base_folder, image_name, "json_grype.json"), image_name]
     subprocess.run(command_grype_sbom, capture_output=True, text=True, check=True)
 
-    command_docker_sbom = ["docker", "sbom", "--format", "spdx-json", "--output", os.path.join("results", image_name, "sbom_docker.json"), image_name]
+    command_docker_sbom = ["docker", "sbom", "--format", "spdx-json", "--output", os.path.join(base_folder, image_name, "sbom_docker.json"), image_name]
     subprocess.run(command_docker_sbom, capture_output=True, text=True, check=True)
 
-    command_scout_scout = ["docker", "scout", "sbom", "--format", "spdx", "--output", os.path.join("results", image_name, "sbom_scout.json"), image_name]
+    command_scout_scout = ["docker", "scout", "sbom", "--format", "spdx", "--output", os.path.join(base_folder, image_name, "sbom_scout.json"), image_name]
     subprocess.run(command_scout_scout, capture_output=True, text=True, check=True)
 
 def get_component_info_grype_json(image: str, file_name: str, outfile_name: str):
-    with open(os.path.join("results", image, file_name), 'r') as file:
+    with open(os.path.join(base_folder, image, file_name), 'r') as file:
         data = json.load(file)
 
-    with open(os.path.join("results", image, "temp.txt"), 'w') as outfile:
+    with open(os.path.join(base_folder, image, "temp.txt"), 'w') as outfile:
         for match in data['matches']:
             package = match['artifact']
             outfile.write(f"{package['name']} {package['version']} {package['type']}\n")
 
     seen_lines = set()
 
-    with open(os.path.join("results", image, "temp.txt"), 'r') as input_f, open(os.path.join("results", image, outfile_name), 'w') as output_f:
+    with open(os.path.join(base_folder, image, "temp.txt"), 'r') as input_f, open(os.path.join(base_folder, image, outfile_name), 'w') as output_f:
         for line in input_f:
             line = line.strip()
             if line not in seen_lines:
                 output_f.write(line + '\n')
                 seen_lines.add(line)
 
-    with open(os.path.join("results", image, outfile_name), 'r') as file:
+    with open(os.path.join(base_folder, image, outfile_name), 'r') as file:
         lines = file.readlines()
         print(f"grype components identified: {len(lines)}")
 
 def get_component_info_spdx_sbom(image: str, file_name: str, outfile_name: str):
-    with open(os.path.join("results", image, file_name), 'r') as file:
+    with open(os.path.join(base_folder, image, file_name), 'r') as file:
         data = json.load(file)
 
-    with open(os.path.join("results", image,"temp.txt"), 'w') as outfile:
+    with open(os.path.join(base_folder, image,"temp.txt"), 'w') as outfile:
         for package in data['packages']:
             outfile.write(f"{package['name']} {package['versionInfo']}\n")
 
     seen_lines = set()
 
-    with open(os.path.join("results", image, "temp.txt"), 'r') as input_f, open(os.path.join("results", image, outfile_name), 'w') as output_f:
+    with open(os.path.join(base_folder, image, "temp.txt"), 'r') as input_f, open(os.path.join(base_folder, image, outfile_name), 'w') as output_f:
         for line in input_f:
             line = line.strip()
             if line not in seen_lines:
                 output_f.write(line + '\n')
                 seen_lines.add(line)
 
-    with open(os.path.join("results", image, outfile_name), 'r') as file:
+    with open(os.path.join(base_folder, image, outfile_name), 'r') as file:
         lines = file.readlines()
         return len(lines)
 
@@ -69,10 +69,10 @@ def parse_file(filename):
     return packages
 
 def create_summary(image):
-    files = [os.path.join("results", image, 'components_docker.txt'),
-             os.path.join("results", image, 'components_scout.txt'),
-             os.path.join("results", image, 'components_syft.txt')]
-    output_file = os.path.join("results", image, "summary.md")
+    files = [os.path.join(base_folder, image, 'components_docker.txt'),
+             os.path.join(base_folder, image, 'components_scout.txt'),
+             os.path.join(base_folder, image, 'components_syft.txt')]
+    output_file = os.path.join(base_folder, image, "summary.md")
 
     def parse_file(filename):
         return {package.strip(): True for package in open(filename)}
@@ -106,11 +106,11 @@ def create_summary(image):
     print("Summary written to", output_file)
 
 def create_diffs(image):
-    input_file_path = os.path.join("results", image, "summary.md")
-    no_output_file_path = os.path.join("results", image,"summary_some_no.txt")
-    yes_output_file_path = os.path.join("results", image, "summary_all_yes.txt")
-    two_yes_output_file_path = os.path.join("results", image,"summary_two_yes.txt")
-    two_no_output_file_path = os.path.join("results", image, "summary_two_no.txt")
+    input_file_path = os.path.join(base_folder, image, "summary.md")
+    no_output_file_path = os.path.join(base_folder, image,"summary_some_no.txt")
+    yes_output_file_path = os.path.join(base_folder, image, "summary_all_yes.txt")
+    two_yes_output_file_path = os.path.join(base_folder, image,"summary_two_yes.txt")
+    two_no_output_file_path = os.path.join(base_folder, image, "summary_two_no.txt")
 
     with open(input_file_path, "r") as input_file, open(no_output_file_path, "w") as output_file:
         for line in input_file:
@@ -141,6 +141,7 @@ def do_counts(input_file):
 
 #samples with one docker image for now
 image = "python:3.12-slim-bookworm"
+
 images =["python:3.12-slim-bookworm",
          "node:18.16.1-alpine",
          "rabbitmq:3.12.12-management",
@@ -158,6 +159,13 @@ images =["python:3.12-slim-bookworm",
          "python:3.13-slim-bookworm",
          "almalinux:8.9-minimal",
          "kartoza/geoserver:2.11.2"]
+
+#for image in images:
+#    print(f"Processing image: {image}")
+
+base_folder = "results"
+
+results_location = os.path.join(base_folder, image)
 get_sboms_for_images(image)
 get_component_info_grype_json(image, "json_grype.json", "components_grype.txt")
 syft = get_component_info_spdx_sbom(image, "sbom_syft.json", "components_syft.txt")
@@ -168,7 +176,7 @@ scout = get_component_info_spdx_sbom(image, "sbom_scout.json", "components_scout
 print (f"scout components identified: {scout}")
 create_summary(image)
 create_diffs(image)
-print(f'Number of components found by all three: {do_counts(os.path.join("results", image,"summary_all_yes.txt"))}')
-print(f'Number of components missed by two tools: {do_counts(os.path.join("results", image, "summary_two_no.txt"))}')
-print(f'Number of components missed by some tool: {do_counts(os.path.join("results", image, "summary_some_no.txt"))}')
-print(f'Number of components found two tools: {do_counts(os.path.join("results", image, "summary_two_yes.txt"))}')
+print(f'Number of components found by all three: {do_counts(os.path.join(results_location,"summary_all_yes.txt"))}')
+print(f'Number of components missed by two tools: {do_counts(os.path.join(results_location, "summary_two_no.txt"))}')
+print(f'Number of components missed by some tool: {do_counts(os.path.join(results_location, "summary_some_no.txt"))}')
+print(f'Number of components found two tools: {do_counts(os.path.join(results_location, "summary_two_yes.txt"))}')
